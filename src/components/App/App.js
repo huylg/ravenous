66d6d7a78c92e3b3e5438790242d4c9e9ee87740
Huylg <lygiahuy11111998.hcmus@gmail.com>
2020-10-12 18:36:12 +0700

    finish stage 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,33 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar.js';
 import BusinessList from '../BusinessList/BusinessList.js';
 
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [];
+
+businesses.push(business);
+
+for(let i=0;i<=4;i++){
+  const item = Object.assign({},business);
+  businesses.push(item);
+}
+
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
