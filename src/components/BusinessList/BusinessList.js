cd2298bdb0512352eaed3aa84b1cae2fa4ce8b8a
Huylg <lygiahuy11111998.hcmus@gmail.com>
2020-10-12 15:22:59 +0700

    finish stage 1

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,17 @@
+import './BusinessList.css';
+import React from  'react';
+import Business from '../Business/Business.js';
+
+class BusinessList extends React.Component {
+  render(){
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
+}
+export default BusinessList;
